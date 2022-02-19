5cb41086958404edfd3064153e8a596d087ea7bb
Yota0350 <mori0124running@gmail.com>
2022-02-19 13:59:00 +0900

    first commit

---
+++ Study_MyOkashi/Study_MyOkashiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Study_MyOkashiApp.swift
+//  Study_MyOkashi
+//
+//  Created by Yota on 2022/02/19.
+//
+
+import SwiftUI
+
+@main
+struct Study_MyOkashiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
